f35f8d6f6f55de6c2760cf936d386bda28907490
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2018-10-20 19:50:30 +0500

    Add example text and test it

--- identificator_analyzer/main.cpp
+++ identificator_analyzer/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include "file_reader.h"
+#include "simple_parser.h"
 
 int main()
 {
+	file_reader reader("example.txt");
+	simple_parser parser;
+	auto result = parser.parse_all(reader);
+	for (const auto& it : result.get_words_count())
+	{
+		for (const auto& c : it.first)
+		{
+			std::cout << c;
+		}
+		std::cout << " " << it.second << std::endl;
+	}
 }
