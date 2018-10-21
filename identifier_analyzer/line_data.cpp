f96a3d27a45fadab55f73ffc3d774a8d8eb77786
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2018-10-21 10:29:53 +0500

    Remove unused methods and variables

--- identifier_analyzer/line_data.cpp
+++ identifier_analyzer/line_data.cpp
@@ -1,11 +1,5 @@
 #include "line_data.h"
 
-void line_data::add_introduced_variable(const std::string& name)
-{
-	introduced_variables.emplace(name);
-	add_used_variable(name);
-}
-
 void line_data::add_used_variable(const std::string& name)
 {
 	++used_variables[name];
