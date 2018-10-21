f15976a58046715f92a1730ec389c7831fda1775
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2018-10-21 07:55:42 +0500

    Delete simple parser files

--- identificator_analyzer/main.cpp
+++ identificator_analyzer/main.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
-#include "file_reader.h"
-#include "simple_parser.h"
 
 int main()
 {
-	file_reader reader("example.txt");
-	simple_parser parser;
-	auto result = parser.parse_all(reader);
-	for (const auto& it : result.get_all_words())
-	{
-		for (const auto& c : it.first)
-		{
-			std::cout << c;
-		}
-		std::cout << " " << it.second << std::endl;
-	}
 }
