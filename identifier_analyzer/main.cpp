c07d65569c7f36174a8cad5124d3504231748e5c
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2018-11-13 21:09:38 +0500

    Choose input/output file names

--- identifier_analyzer/main.cpp
+++ identifier_analyzer/main.cpp
@@ -4,8 +4,23 @@
 
 int main()
 {
-	const auto input = "example.txt";
-	const auto output = "output.txt";
+	std::string input = "input.txt";
+	std::string output = "output.txt";
+
+	std::string buffer;
+	std::cout << "Input file (leave empty for default): ";
+	std::cin >> buffer;
+	if (!buffer.empty())
+	{
+		input = buffer;
+		buffer.clear();
+	}
+	std::cout << "Output file (leave empty for default): ";
+	std::cin >> buffer;
+	if (!buffer.empty())
+	{
+		output = buffer;
+	}
 
 	file_parser parser(input);
 	auto result = parser.get_user_defined_words();
